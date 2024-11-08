653f4cf15d3ba99525d554dffe281414a9312fbc
singhnarendra-nsr <nsr9602448@gmail.com>
2024-11-09 02:22:41 +0530

    modify2

--- app.py
+++ app.py
@@ -47,8 +47,5 @@ def predict():
     return render_template('index.html', prediction=classification)
 
 
-import os
-
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(debug=True)
