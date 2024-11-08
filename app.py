b1439f3738f8ab5d50b2770c1659352d1c89afae
singhnarendra-nsr <nsr9602448@gmail.com>
2024-11-08 19:59:03 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request
+import cv2
+import pickle
+
+# Load the model
+with open('cnn_model3.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def hello_word():
+    return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def predict():
+    imagefile = request.files['imagefile']
+    image_path = "./images/" + imagefile.filename
+    imagefile.save(image_path)
+
+    # Load the image using OpenCV
+    test_img = cv2.imread(image_path)
+
+    # Resize the image
+    test_img = cv2.resize(test_img, (256, 256))
+
+    # Prepare the image for the model
+    test_input = test_img.reshape((1, 256, 256, 3))
+
+    # Make a prediction using the model
+    a = model.predict(test_input)
+    a = a[0][0]
+
+    # Calculate the percentage for real and edited
+    real_percentage = round(a * 100, 2)  # Percentage of being a real image
+    edited_percentage = round((1 - a) * 100, 2)  # Percentage of being an edited image
+
+    # Determine the classification
+    if a > 0.5:
+        classification = f"Real Image ({real_percentage}%)"
+    else:
+        classification = f"Edited Image ({edited_percentage}%)"
+
+    # Pass the information to the template
+    return render_template('index.html', prediction=classification)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
